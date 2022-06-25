5699ec994305e6b70e9c5457212c69e0136cf012
Lucid <lucid@Yahirs-MacBook-Air.local>
2022-06-24 20:59:31 -0700

    Added if user wants numbers question

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ var numbers = "0123456789";
 var upLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var downLetter = "abcdefghijklmnopqrstuvwxyz";
 var passLength;
+var checknum;
 
-
-
+//Function that will prompt user to input desired password lenght
 function inputLength() {
   passLength = prompt("Input desired password length 8-128 Characters.");
 
@@ -18,6 +18,38 @@ if (passLength<8){
   inputLength();
 }
 }
+
+//function to ask user if they want numbers in their password.
+
+function wantNumber(){
+    checknum = prompt("Do you want the password to include numbers? \n (N = No or Y = Yes");
+    checknum = checknum.toLowerCase();
+
+if (checknum === null || checknum === ""){
+  alert("Invalid Answer Try again.")
+  wantNumber();
+
+}else if (checknum === "y" || checknum === "yes"){
+checknum = true;
+return checknum;
+
+
+}else if (checknum === "n" || checknum === "no"){
+  checknum = false;
+  return checknum;
+
+}else {
+  alert("Invalid Answer Try again.");
+  wantNumber();
+}
+return checknum;
+}
+
+
+function checkSpec(){
+  
+}
+
 
 function genPassword(){
 inputLength();
