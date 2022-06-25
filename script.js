87afff924b13fb481420cf98fcdff6bb3178778d
Lucid <lucid@Yahirs-MacBook-Air.local>
2022-06-24 20:46:31 -0700

    Fixed Prompt

--- script.js
+++ script.js
@@ -1,46 +1,42 @@
 // Declared Variables
 var generateBtn = document.querySelector("#generate");
 var numbers = "0123456789";
-var upLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var downLetter = "abcdefghijklmnopqrstuvwxyz"
-var inputLength;
+var upLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var downLetter = "abcdefghijklmnopqrstuvwxyz";
 var passLength;
+
+
 
 function inputLength() {
   passLength = prompt("Input desired password length 8-128 Characters.");
 
-if (passLength <8){
+if (passLength<8){
+  alert("Try again, Desired password must be between 8-128 Characters.");
+  inputLength();
+}else if(passLength>128) {
   alert("Try again, Desired password must be between 8-128 Characters.");
   inputLength();
 }
-else if(passLength>128) {
-  alert("Try again, Desired password must be between 8-128 Characters.");
-  inputLength();
-}
 }
 
+function genPassword(){
+inputLength();
+console.log(passLength);
 
 
-
-
-
-
-
-
-
-
-
-
-
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+var passwords = "";
+   passwords = genPassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+ passwordText.value = passwords;
 }
 
 // Add event listener to generate button
+
+
+
+
 generateBtn.addEventListener("click", writePassword);
